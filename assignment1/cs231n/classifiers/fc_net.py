cec2e57b76cf86da7737f7e3e51b48911e3f2ba5
Ehab <1152012166@qq.com>
2022-08-05 22:12:12 +0800

    two_layer_net

--- assignment1/cs231n/classifiers/fc_net.py
+++ assignment1/cs231n/classifiers/fc_net.py
@@ -91,7 +91,8 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        X1 = X.reshape(X.shape[0], -1).dot(self.params['W1']) + self.params['b1']
+        X = X.reshape(X.shape[0], -1)
+        X1 = X.dot(self.params['W1']) + self.params['b1']
         scores = X1.dot(self.params['W2']) + self.params['b2']
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -120,12 +121,14 @@ class TwoLayerNet(object):
         loss = loss + 0.5 * self.reg * (np.sum(self.params['W1']**2) + np.sum(self.params['W2']**2))
 
         grads['W2'] = X1.T.dot(ds) + self.reg * self.params['W2']
-        grads['b2'] = np.ones((1, ds.shape[0])).dot(ds)
+        # grads['b2'] = np.ones((1, ds.shape[0])).dot(ds).reshape(ds.shape[1])
+        grads['b2'] = np.sum(ds, axis=0)
 
         ds1 = ds.dot(self.params['W2'].T)   # ds1: (N, hidden_dim)
 
         grads['W1'] = X.T.dot(ds1) + self.reg * self.params['W1']
-        grads['b1'] = np.ones((1, ds1.shape[0])).dot(ds1)
+        # grads['b1'] = np.ones((1, ds1.shape[0])).dot(ds1).reshape(ds1.shape[1])
+        grads['b1'] = np.sum(ds1, axis=0)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
--- assignment1/cs231n/optim.py
+++ assignment1/cs231n/optim.py
@@ -69,7 +69,8 @@ def sgd_momentum(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    next_w = w + v
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -107,7 +108,8 @@ def rmsprop(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dw**2
+    next_w = w - config['learning_rate'] * dw /(np.sqrt(config['cache'] + config['epsilon']))
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -152,7 +154,16 @@ def adam(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    config['t'] += 1
+    beta1 = config['beta1']
+    beta2 = config['beta2']
+    epsilon = config['epsilon']
+    learning_rate = config['learning_rate']
+    config['m'] = beta1 * config['m'] + (1-beta1) * dw
+    config['v'] = beta2 * config['v'] + (1-beta2) * dw**2
+    mb = config['m']/(1 - beta1**config['t'])
+    vb = config['v']/(1 - beta2**config['t'])
+    next_w = w - learning_rate * mb / (np.sqrt(vb)+epsilon)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
